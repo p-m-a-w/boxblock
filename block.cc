08c2e7ce1a7f73a0fabdf2e4e3943a22e5e0360f
p-m-a-w <peerapolth3327@hotmail.com>
2022-01-17 19:48:46 +0700

    initial commit

---
+++ block.cc
@@ -0,0 +1,37 @@
+#include <block.hpp>
+#include <memory>
+
+void hash_fn(char data[], char &output, size_t size) {
+    char hash = 0;
+    for (int i = 0; i < size; ++i) {
+        hash ^= data[i];
+    }
+    output = hash;
+}
+
+void hash(block &b) {
+    char t[HASH_SIZE + DATA_SIZE] = {}, hash = 0;
+    memcpy(t, b.depend, HASH_SIZE);
+    memcpy(t + HASH_SIZE, b.data, DATA_SIZE);
+    hash_fn(t, hash, HASH_SIZE + DATA_SIZE);
+    memcpy(b.hash, &hash, HASH_SIZE);
+}
+
+void link(block &current, block &prev) {
+    current.prev = &prev;
+    prev.next = &current;
+    memcpy(current.depend, prev.hash, HASH_SIZE);
+    hash(current);
+}
+
+bool validation(const block *b) {
+    char t[HASH_SIZE + DATA_SIZE] = {}, hash = 0;
+    memcpy(t, b->depend, HASH_SIZE);
+    memcpy(t + HASH_SIZE, b->data, DATA_SIZE);
+    hash_fn(t, hash, HASH_SIZE + DATA_SIZE);
+
+    bool result = !memcmp(&hash, b->hash, HASH_SIZE);
+    if (b->prev == NULL) return result;
+    else if (!result) return result;
+    else return result && validation(b->prev);
+}
---
+++ block.hpp
@@ -0,0 +1,16 @@
+#include <cstddef>
+
+#define HASH_SIZE 1
+#define DATA_SIZE 32
+
+typedef struct _block {
+    char depend[HASH_SIZE] = {};
+    char data[DATA_SIZE] = {};
+    char hash[HASH_SIZE] = {};
+    const _block *prev = NULL;
+    const _block *next = NULL;
+} block;
+
+void hash(block &);
+void link(block &, block &);
+bool validation(const block *);
