45d9ef418542054f3d07f67215da0c6006e2d6a7
Nakamol180 <nakamol_nuang@cmu.ac.th>
2022-02-01 21:55:37 +0700

    impliment new function  - impliment submitVote

--- src/interface.cc
+++ src/interface.cc
@@ -1,4 +1,12 @@
 #include "interface.hpp"
+#include "chain.hpp"
+
+void submitVote(int choice, vector<block> &chain){
+    char data[DATA_SIZE] = {};
+    data[0] = '0' + choice;
+    block vote(data);
+    addBlockToChain(vote, chain);
+}
 
 vector<block> loadRecentChain(int n ,const vector<block> &chain){
     vector<block> recent;
--- src/interface.hpp
+++ src/interface.hpp
@@ -1,5 +1,5 @@
 #pragma once
 #include "block.hpp"
 
-void submitVote(int); // submit -> chain
+void submitVote(int choice, vector<block> &chain); // submit -> chain
 std::vector<block> loadRecentChain(int n ,const vector<block> &chain);
